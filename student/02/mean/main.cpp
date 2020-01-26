67951427e0615094e8343aa72b1cb943a87baba3
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-01-26 13:03:47 +0200

    added user number quarry

--- student/02/mean/main.cpp
+++ student/02/mean/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
-
+using namespace std;
 // Write here a function counting the mean value
 
 int main()
 {
-    std::cout << "From how many integer numbers you want to count the mean value? ";
+  int count = 0;
+  int number = 0;
+  float measurement = 0;
+    cout << "From how many integer numbers you want to count the mean value? ";
+    cin >> count;
+    if ( count < 1 ){
+      cout << "Cannot count mean value from " << count << " numbers" << endl;
+    } else {
+      while (number < count){
+	number += 1;
+	cout << "Input " << number << ". number: ";
+	cin >> measurement;
+      }
+    }
 }

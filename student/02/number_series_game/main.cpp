29397823f83444c93d2d791e1ea30e17355d1121
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-01-26 12:38:56 +0200

    Added processing numbers that are divisible by 3 and 7

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -11,6 +11,18 @@ int main()
     int i = 0;
     while(i < number){
       ++i;
-      cout << i << endl;
+
+      if ( i % 3 == 0 and i % 7 == 0){
+	cout << "zip boing" << endl;
+      } else if( i % 7 == 0) {
+      cout << "boing" << endl;  
+      } else if ( i % 3 == 0 ){
+	cout << "zip" << endl;
+      } else {
+	cout << i << endl;
+      }
     }
+    
+  
+    return 0;
 }

e5a88f4605bb3c7615490936375eabdd4f613966
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-01-26 12:19:52 +0200

    first step

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+using namespace std;
 
 int main()
 {
+    int number = 0;
     std::cout << "How many numbers would you like to have? ";
+    cin >> number;
+    
+    int i = 0;
+    while(i < number){
+      ++i;
+      cout << i << endl;
+    }
 }

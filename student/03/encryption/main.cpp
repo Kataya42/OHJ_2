e83077f3d1fa43c3dee7d5c2262030fec841979b
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-02-02 14:17:50 +0200

    Added encryption key checker

--- student/03/encryption/main.cpp
+++ student/03/encryption/main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <cstring>
+#include <cctype>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+
     string key;
     string text;
+
     cout << "Enter the encryption key: ";
     cin >> key;
+
+    if (key.length() != 26){
+        cout << "Error! The encryption key must contain 26 characters." << endl;
+        }
+    cout << endl;
+    for( char& i : key){
+        if (isupper(i)){
+            cout << "Error! The encryption key must contain only lower case characters.";
+      }}
+    cout << endl;
+
+    for( char letter = 'a'; letter < 'z'; ++letter ){
+        string::size_type test = key.find(letter);
+        if (test == string::npos){
+            cout << "Error! The encryption key must contain all alphabets a-z." << endl;
+        } else {
+            cout << "yup" << endl;
+        }
+
+    }
+    cout << endl;
+
+
+
+ //     else if (){
+ //       cout << "Error! The encryption key must contain only lower case characters." << endl;
+ //
+ //   }
     cout << "Enter the text to be encrypted: ";
     cin >> text;
 

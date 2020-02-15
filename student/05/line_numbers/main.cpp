472cae02753edb928ecda071e11b4937e66b3c95
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-02-15 13:13:05 +0200

    Finished line numbering

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<string> lines;
+    string input = "";
+    string output = "";
+    cout << "Input file: ";
+    getline(cin, input);
+    cout << "Output file: ";
+   getline (cin, output);
+
+    ifstream file_object(input);
+    if (not file_object) {
+        cout << "Error! The file " << input <<  " cannot be opened." << endl;
+    } else {
+        string stuf;
+        while (getline(file_object, stuf)){
+            lines.push_back(stuf);
+        }
+    }
+
+    int num = 1;
+    ofstream outfile(output);
+    for (auto s : lines){
+        outfile << num << " "<< s << endl;
+        num++;
+    }
+    outfile.close();
+
     return 0;
 }

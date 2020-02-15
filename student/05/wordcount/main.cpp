dae45639d32df0288735b812f4b8080f617f0186
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-02-15 20:38:02 +0200

    now working, hopefully.

--- student/05/wordcount/main.cpp
+++ student/05/wordcount/main.cpp
@@ -10,13 +10,9 @@ using namespace std;
 int main()
 {
     map<string,vector<int>> occurance;
-
-    string input = "hell.txt";
-    vector<string> words;
-    vector<string> lines;
-    //vector<string> locations;
-    //cout << "Input file: ";
-    //getline(cin, input);
+    string input = "";
+    cout << "Input file: ";
+    getline(cin, input);
 
     ifstream file_object(input);
     if (not file_object) {
@@ -29,11 +25,11 @@ int main()
        char sep = ' ';
 
         while (getline(file_object, stuf)){
-            vector<int> rows;
+
             string temp;
             for (char c : stuf){
+                vector<int> rows;
                 if (c == sep){
-                    // words.push_back(temp);
                     if (occurance.find(temp) != occurance.end()) {
                         // found
                         occurance[temp].push_back(line);
@@ -42,9 +38,6 @@ int main()
                         rows.push_back(line);
                         occurance[temp] = rows;
                     }
-
-                    //rows.push_back(line);
-                    //occurance[temp] = rows;
                     temp = "";
                 } else {
                     temp += c;
@@ -56,44 +49,27 @@ int main()
                 occurance[temp].push_back(line);
             } else {
                 // not found, make new
+                vector<int> rows;
                 rows.push_back(line);
                 occurance[temp] = rows;
             }
-            //words.push_back(temp);
-            // string test = to_string(line_by);
-            // stuf += test;
-            //lines.push_back(stuf);
+
             line ++;
         }
 
-//        for (auto i : words){
-//            if ( occurance.find(i) != occurance.end() ) {
-//                // The word was found in the map.
-//                occurance[i] = occurance[i]+1;
-//            } else {
-//                // The word was not found in the map.
-//                occurance[i] = 1;
-//            }
-
-//        }
-
-//        for (auto c: occurance){
-//            if (find(words_s.begin(), words_s.end(), c.first) != words_s.end()){
-//                cout << "yes" << endl;
-//           } else {
-//               cout << "no" << endl;
-//            }
-
-//        }
-
-
-
-
-//        for (auto c : lines){
-//            cout << c << endl;
-//        }
        for (auto c : occurance){
-           cout << c.first << " " << c.second.size() << ":" << endl;
+           string locations;
+            for (unsigned int i=0; i<c.second.size(); i++){
+               locations += to_string(c.second[i]);
+               if (i == c.second.size()-1){
+
+               } else {
+                   locations += ", ";
+               }
+
+
+           }
+           cout << c.first << " " << c.second.size() << ": " << locations << endl;
        }
        }
 

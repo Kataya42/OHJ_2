7448c545c9a06e1840767bb995b0186bcd8d9aeb
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-03-01 15:24:39 +0200

    Initial commit, hope it works

--- student/06/network/main.cpp
+++ student/06/network/main.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+
+using namespace std;
 
 const std::string HELP_TEXT = "S = store id1 i2\nP = print id\n"
                               "C = count id\nD = depth id\n";
-
 
 std::vector<std::string> split(const std::string& s, const char delimiter, bool ignore_empty = false){
     std::vector<std::string> result;
@@ -27,13 +29,40 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     return result;
 }
 
+map<string,vector<string>> dudes;
+int max_depth = 0;
+int size = 0;
 
+void print_me_and_underlings(int depth, string me) {
+    for (int i=0; i < depth; ++i){
+        cout << "..";
+    }
+    depth++;
+    cout << me << endl;
+    for (string dude : dudes[me]) {
+        print_me_and_underlings(depth, dude);
+    }
+}
+
+void count_me_and_underlings(int depth, string me) {
+    int my_depth = depth;
+    my_depth++;
+    for (string dude : dudes[me]) {
+       count_me_and_underlings(my_depth, dude);
+       if (my_depth > max_depth)
+           max_depth = my_depth;
+    }
+    my_depth--;
+}
+
+void depth_me_and_underlings(string me) {
+    size++;
+    for (string dude : dudes[me])
+       depth_me_and_underlings(dude);
+}
 
 int main()
 {
-    // TODO: Implement the datastructure here
-
-
     while(true){
         std::string line;
         std::cout << "> ";
@@ -46,11 +75,15 @@ int main()
             if(parts.size() != 3){
                 std::cout << "Erroneous parameters!" << std::endl << HELP_TEXT;
                 continue;
-            }
-            std::string id1 = parts.at(1);
-            std::string id2 = parts.at(2);
+            } else {
 
-            // TODO: Implement the command here!
+                string id1 = parts.at(1);
+                string id2 = parts.at(2);
+
+                if (dudes.find(id1) == dudes.end())
+                    dudes[id1];
+                dudes[id1].push_back(id2);
+            }
 
         } else if(command == "P" or command == "p"){
             if(parts.size() != 2){
@@ -58,8 +91,7 @@ int main()
                 continue;
             }
             std::string id = parts.at(1);
-
-            // TODO: Implement the command here!
+            print_me_and_underlings(0, id);
 
         } else if(command == "C" or command == "c"){
             if(parts.size() != 2){
@@ -67,8 +99,15 @@ int main()
                 continue;
             }
             std::string id = parts.at(1);
+            size = -1;
+            if (dudes.find(id) == dudes.end()){
+                cout << "0" << endl;
+            } else {
+                depth_me_and_underlings(id);
+                cout << size << endl;
+            }
 
-            // TODO: Implement the command here!
+
 
         } else if(command == "D" or command == "d"){
             if(parts.size() != 2){
@@ -77,7 +116,9 @@ int main()
             }
             std::string id = parts.at(1);
 
-            // TODO: Implement the command here!
+            max_depth = 1;
+            count_me_and_underlings(1, id);
+            cout << max_depth << endl;
 
         } else if(command == "Q" or command == "q"){
            return EXIT_SUCCESS;

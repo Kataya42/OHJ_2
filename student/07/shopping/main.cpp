74512b2667ecc549c62a2b4970cd7b418988b406
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-03-07 17:38:33 +0200

    cleaned up menu function into smaller functions, added more precise error checking

--- student/07/shopping/main.cpp
+++ student/07/shopping/main.cpp
@@ -37,15 +37,13 @@ std::vector<std::string> split(const std::string& s, const char delimiter, bool 
     }
     return result;
 }
-
 struct Product {
     std::string name;
     double price;
 };
-
 void inputFile( std::map< std::string,std::map < std::string, std::map <std::string, Product>> > &all, const std::vector<std::string> &line) {
 
-    double price = -1.0;
+    double price = 98;
     if ( line[3] != "out-of-stock")
         price = stod(line[3]);
 
@@ -57,82 +55,112 @@ void inputFile( std::map< std::string,std::map < std::string, std::map <std::str
     //all[line[0]].insert({line[1],{}});
     all[line[0]][line[1]].insert({line[2],pro});
 }
+bool correctArgs(const std::vector<std::string> &args, const unsigned int &correctNumber){
+    if (args.size() == correctNumber){
+        return true;
+    } else {
+        std::cout << "Error: error in command " << args[0] << std::endl;
+        return false;
+    }
 
+}
+void Findcheapest(const std::map< std::string,std::map < std::string, std::map <std::string, Product>> > &all,const std::vector<std::string> &args){
+    //std::cout << "cheapest products of selected name" << std::endl;
+    double cheapestPrice = 99;
+    for (auto c : all){
+        for (auto v : c.second){
+            if (v.second[args[1]].price > 0 && v.second[args[1]].price < cheapestPrice){
+                cheapestPrice = v.second[args[1]].price;
 
-void menu(std::map< std::string,std::map < std::string, std::map <std::string, Product>> > &all) {
+            }
+        }
+    }
+
+    if (cheapestPrice == 99) {
+        std::cout << "The product is not part of product selection" << std::endl;
+    } else if (cheapestPrice == 98){
+        std::cout << "The product is temporarily out of stock everywhere" << std::endl;
+    }else {
+        std::cout << cheapestPrice << " euros" << std::endl;
+
+        for (auto c : all){
+            for (auto v : c.second){
+                if (v.second[args[1]].price == cheapestPrice){
+                    std::cout << c.first << " " << v.first << std::endl;
+                }
+            }
+        }
+    }
+}
+void findProducts(const std::map< std::string,std::map < std::string, std::map <std::string, Product>> > &all){
+    //std::cout << "here are all prodcuts there are" << std::endl;
+    std::vector<std::string> foundItems;
+    for (auto c : all){
+        for (auto v : c.second){
+            for (auto b : v.second){
+                if (std::find(foundItems.begin(), foundItems.end(), b.second.name) != foundItems.end())
+                {
+
+                } else {
+                    foundItems.push_back(b.second.name);
+                    std::cout << b.second.name << std::endl;
+                }
+
+            }
+        }
+
+    }
+
+}
+void menu( std::map< std::string,std::map < std::string, std::map <std::string, Product>> > &all) {
 
     std::string input;
-    while (input != "quit"){
+    while (input != "quit") {
         std::cout << "> ";
         getline(std::cin, input);
         std::vector<std::string> args = split(input, ' ', true);
 
-        if (args[0] == "chains"){
-            //std::cout << "here are the available store chains" << std::endl;
-            for (auto c : all)
-                std::cout << c.first << std::endl;
-
+        if (args[0] == "chains") {
+            if (correctArgs(args, 1)){
+                for (auto c : all)
+                    std::cout << c.first << std::endl;
+            }
         } else if (args[0] == "stores") {
-            //std::cout << "here are the stores of a certain chain" << std::endl;
-            for (auto c : all[args[1]])
-                std::cout << c.first << std::endl;
 
+             if (correctArgs(args, 2)) {
+                 if ( all.find(args[1]) == all.end() ) {
+                   std::cout << "Error: unknown chain name" << std::endl;
+                 } else {
+                     for (auto c : all[args[1]])
+                         std::cout << c.first << std::endl;
+                 }
+            }
         } else if (args[0] == "selection") {
-            //std::cout << "here is the selection of a store" << std::endl;
-            for (auto c : all[args[1]][args[2]])
-                std::cout << c.second.name << " " << c.second.price <<std::endl;
-
+             if (correctArgs(args, 3)) {
+                 if ( all.find(args[1]) == all.end() ) {
+                   std::cout << "Error: unknown chain name" << std::endl;
+                 } else if (all[args[1]].find(args[2]) == all[args[1]].end()){
+                   std::cout << "Error: unknown store" << std::endl;
+                 } else {
+                    for (auto c : all[args[1]][args[2]])
+                        std::cout << c.second.name << " " << c.second.price <<std::endl;
+                 }
+             }
         } else if (args[0] == "cheapest") {
-            //std::cout << "cheapest products of selected name" << std::endl;
-            double cheapestPrice = 99;
-            for (auto c : all){
-                for (auto v : c.second){
-                    if (v.second[args[1]].price > 0 && v.second[args[1]].price < cheapestPrice){
-                        cheapestPrice = v.second[args[1]].price;
-                    }
-
-
-                }
-            }
-            std::cout << cheapestPrice << " euros" << std::endl;
-            for (auto c : all){
-                for (auto v : c.second){
-                    if (v.second[args[1]].price == cheapestPrice){
-                        std::cout << c.first << " " << v.first << std::endl;
-                    }
-                }
-            }
-
+             if (correctArgs(args, 2))
+                Findcheapest(all, args);
 
         } else if (args[0] == "products") {
-            //std::cout << "here are all prodcuts there are" << std::endl;
-            std::vector<std::string> foundItems;
-            for (auto c : all){
-                for (auto v : c.second){
-                    for (auto b : v.second){
-                        if (std::find(foundItems.begin(), foundItems.end(), b.second.name) != foundItems.end())
-                        {
-
-                        } else {
-                            foundItems.push_back(b.second.name);
-                            std::cout << b.second.name << std::endl;
-                        }
-
-                    }
-                }
-
-            }
-
-
+            if (correctArgs(args, 1))
+                findProducts(all);
 
         }  else if (args[0] != "quit") {
-            std::cout << "Error: unknown command: " << args[0] << std::endl;
+             if (correctArgs(args, 1))
+                std::cout << "Error: unknown command: " << args[0] << std::endl;
         }
     }
 }
-
-int main()
-{
+int main() {
     std::string input = "products.input";
 
     std::map< std::string,std::map < std::string, std::map <std::string, Product>> > all;
@@ -154,7 +182,6 @@ int main()
     }
 
     menu(all);
-    // std::cout << all["Prisma"]["Lielahti"]["sausage"].name << " " << all["Prisma"]["Lielahti"]["sausage"].price << std::endl;
 
     return EXIT_SUCCESS;
 }

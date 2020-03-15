8255cff11ba84f4867f88f19d765333b23412b95
Aleksi Rissa <aleksi.rissa@tuni.fi>
2020-03-15 15:29:38 +0200

    fixed another bug with reverse function

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -39,7 +39,7 @@ void copy(int *itemptr, int *endptr, int *targetptr){
 
 void reverse(int *leftptr, int *rightptr){
     rightptr--;
-    while (*leftptr < *rightptr){
+    while (leftptr < rightptr){
         int temp = *leftptr;
         *leftptr = *rightptr;
         *rightptr = temp;
